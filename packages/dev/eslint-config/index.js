5a271cb3b61cecbaeb3fe3c040e584309975ca86
Bogdan Savluk <savluk.bogdan@gmail.com>
2020-11-15 06:11:31 +0100

    eslint update eslint config

--- packages/dev/eslint-config/index.js
+++ packages/dev/eslint-config/index.js
@@ -1,15 +1,22 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:flowtype/recommended',
+    // 'plugin:flowtype/recommended',
     'plugin:monorepo/recommended',
     'plugin:react/recommended',
     'prettier',
-    'prettier/flowtype',
+    // 'prettier/flowtype',
     'prettier/react',
   ],
   parser: '@babel/eslint-parser',
-  plugins: ['@parcel', 'flowtype', 'import', 'monorepo', 'react', 'mocha'],
+  plugins: [
+    '@parcel',
+    //'flowtype',
+    'import',
+    'monorepo',
+    'react',
+    'mocha',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     ecmaFeatures: {
@@ -29,12 +36,28 @@ module.exports = {
   // https://eslint.org/docs/user-guide/configuring#configuration-based-on-glob-patterns
   overrides: [
     {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'import/first': 'off',
+        'import/newline-after-import': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "no-dupe-class-members": "off",
+        "@typescript-eslint/no-dupe-class-members": "error",
+        "no-undef": "off",
+        // "no-redeclare": "off",
+      },
+    },
+    {
       files: ['**/test/**', '*.test.js', 'packages/core/integration-tests/**'],
       env: {
         mocha: true,
       },
       rules: {
-        'import/no-extraneous-dependencies': 'off',
+        //'import/no-extraneous-dependencies': 'off',
         'monorepo/no-internal-import': 'off',
         'monorepo/no-relative-import': 'off',
         'mocha/no-exclusive-tests': 'error',
@@ -45,7 +68,7 @@ module.exports = {
     '@parcel/no-self-package-imports': 'error',
     'import/first': 'error',
     'import/newline-after-import': 'error',
-    'import/no-extraneous-dependencies': 'error',
+    'import/no-extraneous-dependencies': 'off',
     'import/no-self-import': 'error',
     'no-prototype-builtins': 'off',
     'no-console': 'error',
