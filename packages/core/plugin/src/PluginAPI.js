29e754f3eb52a8303b6b67504a3fc26826a6f6c4
Bogdan Savluk <savluk.bogdan@gmail.com>
2021-06-22 13:37:26 +0200

    fix class type parameters

--- packages/core/plugin/src/PluginAPI.js
+++ packages/core/plugin/src/PluginAPI.js
@@ -15,8 +15,8 @@ import type {
 
 const CONFIG = Symbol.for('parcel-plugin-config');
 
-export class Transformer {
-  constructor<T>(opts: TransformerOpts<T>) {
+export class Transformer<T> {
+  constructor(opts: TransformerOpts<T>) {
     // $FlowFixMe
     this[CONFIG] = opts;
   }
@@ -29,22 +29,22 @@ export class Resolver {
   }
 }
 
-export class Bundler {
-  constructor<T>(opts: BundlerOpts<T>) {
+export class Bundler<T> {
+  constructor(opts: BundlerOpts<T>) {
     // $FlowFixMe
     this[CONFIG] = opts;
   }
 }
 
-export class Namer {
-  constructor<T>(opts: NamerOpts<T>) {
+export class Namer<T> {
+  constructor(opts: NamerOpts<T>) {
     // $FlowFixMe
     this[CONFIG] = opts;
   }
 }
 
-export class Runtime {
-  constructor<T>(opts: RuntimeOpts<T>) {
+export class Runtime<T> {
+  constructor(opts: RuntimeOpts<T>) {
     // $FlowFixMe
     this[CONFIG] = opts;
   }
@@ -57,15 +57,15 @@ export class Validator {
   }
 }
 
-export class Packager {
-  constructor<T, U>(opts: PackagerOpts<T, U>) {
+export class Packager<T, U> {
+  constructor(opts: PackagerOpts<T, U>) {
     // $FlowFixMe
     this[CONFIG] = opts;
   }
 }
 
-export class Optimizer {
-  constructor<T, U>(opts: OptimizerOpts<T, U>) {
+export class Optimizer<T, U> {
+  constructor(opts: OptimizerOpts<T, U>) {
     // $FlowFixMe
     this[CONFIG] = opts;
   }
