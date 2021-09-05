49d1506bfa5545f8679baa4526c4116b949dc121
Bogdan Savluk <savluk.bogdan@gmail.com>
2021-09-05 02:22:56 +0200

    fixes in hash util

--- packages/utils/hash/index.d.ts
+++ packages/utils/hash/index.d.ts
@@ -3,6 +3,7 @@ export declare function hashString(s: string): string;
 export declare function hashBuffer(b: Buffer): string;
 
 export declare class Hash {
+  data: Array<Uint8Array>;
   writeString(s: string): void;
   writeBuffer(b: Buffer): void;
   finish(): string;
