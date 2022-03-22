9e75c61b9f7072b48fe3e69b29493b28e0152f5a
Bogdan Savluk <savluk.bogdan@gmail.com>
2022-03-22 16:58:46 +0100

    update flowts.sh

--- flowts.sh
+++ flowts.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 flowts \
-  --interactive-rename \
+  --commit-rename-command "git add . && git commit --no-verify -m 'flowts rename'" \
   -x core/integration-tests/test/integration/** \
   ./packages/
+
+git add .
+git commit --no-verify -m 'flowts convert'
 
